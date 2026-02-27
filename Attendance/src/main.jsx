af4b2dd07d3222ebc6a1f70673b25a049e28a18a
kajanthann <arulkajanthan904@gmail.com>
2026-02-27 14:39:37 +0530

    updated

--- Attendance/src/main.jsx
+++ Attendance/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/IOT_Biometric_Fingerprint_Attendance_System/">
+    <HashRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
